a7fdd51409780a6a3a010bc05cd303182f7ac32c
Gustavo <gustavolobatoclara@gmail.com>
2025-04-26 07:22:18 +0200

    Create config.py
    Rellena tu apikey,elige el modo de traiding y tu riesgo asumible

---
+++ config.py
@@ -0,0 +1,10 @@
+
+API_KEY = "TU_API_KEY_BINANCE"
+API_SECRET = "TU_API_SECRET_BINANCE"
+
+# Trading mode "aqui selecciones el modo"
+TRADING_MODE = "PAPER"  # O "REAL"
+
+# Parámetros de gestión de riesgo
+MAX_RISK_PER_TRADE_PERCENT = 1  # máximo 1% del balance
+TRAILING_STOP_PERCENT = 0.5  # trailing dinámico de 0.5%
